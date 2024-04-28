f0d5e0c3b93df11ebeb9f07b66edbd7f6d09f79d
smitakhutal <83202718+smitakhutal@users.noreply.github.com>
2024-04-28 16:43:45 +0000

    Project 2 compleat

--- Project-2/BMICalculate.js
+++ Project-2/BMICalculate.js
@@ -1,11 +1,31 @@
-const form=document.querySelector('#btncalculate');
-form.addEventListener('submit',function(e){
+const button=document.querySelector('button');
+let BMI;
+button.addEventListener('click',function(e){
     e.preventDefault()
 
     const height=parseInt(document.querySelector('#txtheight').value)
-    const width=parseInt(document.querySelector('#txtwidth').value)
+    const weight=parseInt(document.querySelector('#txtweight').value)
     const result=document.querySelector('#result')
 
-    console.log(`height:${height} and width:${width}`);
+    if(height=="" || isNaN(height) || height <0 )
+    {
+    result.innerHTML='Please enter valid height'
+    }
+    else if(weight=="" || isNaN(weight) || weight< 0){
+    result.innerHTML='Please enter valid weight'
+    }
+    else{
+    BMI= (weight / ((height * height) / 10000)).toFixed(2)
+    result.innerHTML=`BMI is: ${BMI}`
+    }
+
+    if(BMI <= 18.6)
+    resultguide.innerHTML=`<span>You are under weight</span>`
+    else if(BMI > 18.6 && BMI < 24.9)
+    resultguide.innerHTML=`<span>You ae in normal range</span>`
+    else if(BMI > 24.9)
+    resultguide.innerHTML=`<span>You are overweight </span>`
+
+   
 
 });
